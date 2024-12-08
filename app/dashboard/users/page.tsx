24692a01886592101295a720b4bc3beb30920583
Denis Kotov <deniskotov29042015@gmail.com>
2024-12-08 21:57:34 +0500

    30:00

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,25 @@
-const Page = () => {
-	return (
-		<div>Users</div>
-	)
-}
+import Link from "next/link";
 
-export default Page
+const Page = () => {
+  return (
+    <>
+      <h1>Dashboard Users</h1>
+      <ul className="mt-10">
+        <li>
+          <Link href="/dashboard/users/1">User 1</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/2">User 2</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/3">User 3</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/4">User 4</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Page;
